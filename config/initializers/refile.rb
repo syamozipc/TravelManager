3540dc052fbec10ee19c6c145f9cf8fed3723a0a
vagrant <vagrant@localhost.localdomain>
2020-08-27 13:41:21 +0000

    modified s3 config

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -6,7 +6,6 @@ if Rails.env.production?
     secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1',
     bucket: 'travelmanager-strage',
-    s3_host_name: "s3-ap-northeast-1.amazonaws.com",
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
