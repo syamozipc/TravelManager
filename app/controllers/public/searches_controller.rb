00b16c1b8204b6641393b2095caa9f1ba1f61bcb
RYOTA SAITO <syamozipc@gmail.com>
2020-09-05 10:19:19 +0900

    modified search function

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,6 +2,6 @@ class Public::SearchesController < ApplicationController
   def index
     @word = params[:word]
     @users = User.where('name LIKE ?', "%#{@word}%")
-    @albums = Album.where('title LIKE ? or discription LIKE ?', "%#{@word}%", "%#{@word}%")
+    @albums = Album.publicly_open.where('title LIKE ? or discription LIKE ?', "%#{@word}%", "%#{@word}%")
   end
 end
